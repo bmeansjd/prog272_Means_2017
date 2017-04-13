9f04226cc2eb2f3b4ec5c26db5f14f3b2553f02e
Brenda Means <brenda.means@bellevue.edu>
2017-04-12 17:28:52 -0700

    homework

--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('jest test', function() {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to Prog 272</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+
+    t('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const nineSign = <p className="App-intro">Nine: 9</p>;
+        wrapper.find('button.elf').simulate('click');
+        expect(wrapper.contains(nineSign)).toEqual(true);
+    });
+
+
+
 });
