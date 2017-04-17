76cb0d102bf6633ec08120dcd90f447a034f3bbc
Brenda Means <brenda.means@bellevuecollege.edu>
2017-04-17 15:36:07 -0700

    I am trying to learn git

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();//calling the parent class constructor
+
+        this.state = {
+            nine: '0',
+            firstName: 'Brenda',
+            lastName:''
+        };
+
+        bar = () => {
+
+
+            this.setState({
+                nine: '9',
+                firstName: 'Brenda',
+                lastName: 'Means'
+            })
+
+        };
+        render();
+        {
+
+        }
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Prog 272</h2>
+                </div>
+                <p className="App-intro">
+                    Nine:{this.state.nine}
+                </p>
+                <p className="App-intro">
+                    firstName:{this.state.firstName}
+                </p>
+                <button onClick={this.bar}>Get Nine</button>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
+import {shallow} from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('jReact Jest Suite', function() {
+
+    it('renders without out App component without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to Prog 272</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+see pictures to finih this
+
+   it ('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const nineSign = <p className="App-intro">Nine: 9</p>;
+        wrapper.find('button.elf').simulate('click');
+        expect(wrapper.contains(nineSign)).toEqual(true);
+    });
+
+
+
 });
