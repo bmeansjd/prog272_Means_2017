de3353f6eb8af231e80ad70c9aa19ed6f2a03699
Brenda Means <brenda.means@bellevue.edu>
2017-04-17 19:31:52 -0700

    inclass work

--- week02-react-jest/src/index.js
+++ week02-react-jest/src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import Address from './components/Address';
 import './index.css';
+import Elfheader from ',/Elfheader'
+
+
+
+
+var address =
+{
+
+    firstName : 'foo',
+    lastName : 'bar'
+};
 
 ReactDOM.render(
-  <App />,
+    <div>
+    <Elfheader/>
+        <App/>
+        <Address firstName: 'foobar'/>
+
+
+  </div>,
+
   document.getElementById('root')
 );
