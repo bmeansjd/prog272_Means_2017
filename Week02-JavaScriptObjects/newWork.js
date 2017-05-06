6818f2b565af6cad6c8fb815d8347e9b2500a1d3
Brenda Means <brenda.means@bellevue.edu>
2017-05-06 16:22:11 -0700

    git commit -m "new beginings"

--- Week02-JavaScriptObjects/newWork.js
+++ Week02-JavaScriptObjects/newWork.js
@@ -1,3 +1,78 @@
 /**
  * Created by bcuser on 5/6/17.
  */
+/**
+ * Created by bcuser on 5/6/17.
+ */
+
+
+
+var person = {
+    firstName: 'Brenda',
+    lastName: 'Means',
+    sayName:function() {
+        console.log('Hello, I am ' + this.firstName + ' ' + this.lastName);
+    }
+};
+
+person.sayName();
+
+var  calculator= {
+    operator01: '6',
+    operator02: "5"
+
+};
+calculator.operator02 = person.lastName.length;
+
+calculator.operator01 = person.firstName.length;
+
+
+
+calculator.addName = function () {
+    'use strict';
+    return this.operator01 + this.operator02;
+};
+
+calculator.subName = function () {
+    'use strict';
+    return this.operator01 - this.operator02;
+};
+
+
+calculator.multiplyName=function () {
+    'use strict';
+    return this.operator01 * this.operator02;
+}
+
+
+console.log('operator01 =', calculator.operator01);
+console.log('operator02 =', calculator.operator02);
+console.log('Add: ', calculator.addName());
+console.log('Subtract:', calculator.subName());
+console.log('Multiply:', calculator.multiply());
+
+
+
+/*
+
+
+
+
+
+ // /divider
+ // ('Calculator'); console.log('operator01 =', calculator.operator01); console.log('operator02 =', calculator.operator02); console.log('Add: ', calculator.addName()); console.log('Subtract:', calculator.subName()); console.log('Multiply:', calculator.multiply());
+
+
+
+ calculator.operator01 = person.firstName.length;
+ calculator.operator02 = person.lastName.length;
+ calculator.multiply = function()
+ { 'use strict'; return this.operator01 * this.operator02; };
+
+ //divider('Calculator');
+
+ console.log('operator01 =', calculator.operator01);
+ console.log('operator02 =', calculator.operator02);
+ console.log('Add: ', calculator.addName());
+ console.log('Subtract:', calculator.subName());
+ console.log('Multiply:', calculator.multiply());*/
